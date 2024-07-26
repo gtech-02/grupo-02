fdb121c195d77bf24ac34de8753711afe463d991
Marley Souza <ctt.marley@gmail.com>
2024-07-25 22:50:32 -0300

    Atualização na BuyBox,ProductOptions,Logo,Header,Cadastro,Login

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import ProductListingPage from './pages/ProductListingPage.jsx';
 import ProductViewPage from './pages/ProductViewPage.jsx';
 import Categorias from './pages/Categorias.jsx';
 import MeusPedidos from './pages/MeusPedidos.jsx';
+import Login from './pages/Login.jsx';
+import Cadastro from './pages/Cadastro.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Cadastro" element={<Cadastro />} />
         <Route path="/Produtos" element={<ProductListingPage />} />
         <Route path="/Produtos/:id" element={<ProductViewPage />} />
         <Route path="/Categorias" element={<Categorias />} />
