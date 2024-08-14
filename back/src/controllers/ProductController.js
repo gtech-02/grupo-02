883c7714dfb912970802140bb077da2848b85d9d
VICTOR ALVES DE FREITAS <VICTOR0646@DIGITALCOLLEGE.LOCAL>
2024-08-14 09:46:27 -0300

    Att

--- back/src/controllers/ProductController.js
+++ back/src/controllers/ProductController.js
@@ -1,10 +1,12 @@
 const ProductModel = require('../models/ProductModel');
 const jwt = require('jsonwebtoken');
+const { update } = require('./UserController');
 
 const ProductController = {
     create(request, response) {
         ProductModel.create(request.body);
-        response.json({
+        response.status(201);
+        return response.json({
             message: "Produto criado com sucesso"
         });
     },
@@ -12,6 +14,51 @@ const ProductController = {
     async list(request, response) {
         const products = await ProductModel.findAll();
         response.json(products)
+    },
+
+    async listUm(request,response){
+        let id = request.params.id;
+        let categoria = await ProductModel.findOne({
+            where:{
+                id:id
+            }
+        })
+        return response.json(categoria)
+    },
+
+    async update(request,response){
+        let id = request.params.id;
+        await ProductModel.update(request.body,{
+            where:{
+                id:id
+            }
+        })
+        return response.json({
+            message: "Categoria atualizada com sucesso"
+        })
+    },
+
+    async deleteTodos(request,response){
+        await CategoriaModal.destroy({
+            where:{
+
+            }
+        })
+        return response.json({
+            message: "Todas as categorias foram deletadas"
+        })
+    },
+
+    async deleteUm(request,response){
+        let id = request.params.id;
+        await CategoriaModal.destroy({
+            where:{
+                id:id
+            }
+        })
+        return response.json({
+            message: "A categoria de id "+id+" foi deletada com sucess0"
+        })
     }
 
 }
